4111ef3f5a733db7e69f372ee5fc75b9a99cc0f6
Saravanan Kandaswamy <ksnblr@gmail.com>
2018-06-26 15:59:35 -0400

    commit

--- 02-Flask-Basics/00-Hello_Puppy.py
+++ 02-Flask-Basics/00-Hello_Puppy.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from pyfuncs import *
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello Puppy!</h1>'
-
+@app.route('/page1')
+def pages():
+    return '<h1>Hello page1!</h1>'
+@app.route('/puppy/<name>')
+def puppy(name):
+    new_string = namLatin(name)
+    x = read_data()
+    return x
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
